fb5cd33dd0c80f244f4e1bef86179139a17da4be
Maximilian Kreis <45019337+maksimay@users.noreply.github.com>
2020-12-03 14:36:00 +0100

    updated some funny prints

--- ozkavosh_translator.py
+++ ozkavosh_translator.py
@@ -45,15 +45,23 @@ dict_oz = dict(zipObj)
 r = sr.Recognizer()
 
 with sr.Microphone() as source: 
-    print("SPEAK NOW...")
-    r.adjust_for_ambient_noise(source)
-    data = r.record(source, duration=3)
-    text = r.recognize_google(data)
-    print(text)
+    try:
+        print("SPEAK NOW...")
+        r.adjust_for_ambient_noise(source)
+        data = r.record(source, duration=3)
+        voice_input = r.recognize_google(data)
+        print(voice_input)
+
+    except:
+        print("i couldn't understand you human creature... ")
 
 #Request the voice_input from dictionary 
-translated_text = dict_oz.get(text)
-print(translated_text)
+translated_text = dict_oz.get(voice_input)
+
+if translated_text != None:
+    print(translated_text)
+else:
+    print("We couldnt translate your silly humanish!")
 
 #Play sound when we have the sound 
 if translated_text == "alatho":

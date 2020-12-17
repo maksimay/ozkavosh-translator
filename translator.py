65e65171f5b7a304ba070dabe2bc25a89f45e970
Jannis <jannis@nyx.earth>
2020-12-17 18:58:03 +0100

    added translation dictionary synonym condition

--- translator.py
+++ translator.py
@@ -14,8 +14,6 @@ def get_key(val):
     return "key doesn't exist"
 
 
-
-
 FILEPATH_ENG = "commonwords.txt"
 FILEPATH_OZK = "DemonWords.txt"
 FILEPATH_TRL = "EnglishWords.txt"
@@ -129,85 +127,78 @@ BigList = [i.strip().split() for i in open(FILEPATH_ENG).readlines()]
 
 Translations_Dictionary = {}
 
-Current_Word = []
+Translation_Word = []
 
 
-templist = []
-for key, value in dict_syndLUL.items():                             # for each key value pair item:
-            for i in value:                                         # for each element in the value list:
-                for syn in wordnet.synsets(i):                      # for each synonym in wordnet:
-                    for k in syn.lemmas():                          # for each lemma (actual synonym list word element):
-                        templist.append(k.name())                   # stash the synonym into a temp list!
-            templist = np.unique(templist).tolist()                 # for each key, remove duplicates from templist!
-            for i in templist:                                      # for each element in the stashed synonym list:
-                value.append(i)                                     # append each element to value list in the synonym dict!
-            templist = []                                           # clear the stash for the next iteration!
-
-
-
-
-
-
+temp_list = []
+for key, value in dict_syndLUL.items():                     # for each key value pair item in direct wiki translations:
+    for i in value:                                         # for each element in the value list:
+        for syn in wordnet.synsets(i):                      # for each synonym in wordnet:
+            for k in syn.lemmas():                          # for each lemma (actual synonym list word element):
+                temp_list.append(k.name())                   # stash the synonym into a temp list!
+    temp_list = np.unique(temp_list).tolist()                 # for each key, remove duplicates from temp_list!
+    for i in temp_list:                                      # for each element in the stashed synonym list:
+        value.append(i)                                     # append each element to value list in the synonym dict!
+    temp_list = []                                           # clear the stash for the next iteration!
 
 
 words_to_process = 40
-looprange = range(len(BigList))
-for i in looprange:
+loop_range = range(len(BigList))
+for i in loop_range:
     if i >= words_to_process:
         break
+    else:
+        Translation_Word = str(BigList[i])
+        Translation_Word = [character for character in str.lower(Translation_Word) if character.isalnum()] # clean out [""] etc
+        Translation_Word = "".join(Translation_Word)
+        English_Word = Translation_Word
+        print("en word is", English_Word)
+        # get a string for debug printing
+
+        if English_Word in (item for sublist in dict_syndLUL.values() for item in sublist):
+
+            print("jop da gibts schon ne übersetzung für", English_Word, ":)")
+            # have to do it the other way around because original dict is ozk -> eng but we want eng -> ozk
+            Translations_Dictionary[English_Word] = get_key(English_Word)
+            print(Translations_Dictionary)
 
 
-    Current_Word = str(BigList[i])
-    Current_Word = [character for character in str.lower(Current_Word) if character.isalnum()] # clean out [""] etc
-    Current_Word = "".join(Current_Word)
-    English_Word = Current_Word
-    print("en word is", English_Word)
-    # get a string for debug printing
+        else:
 
-    if English_Word in (item for sublist in dict_syndLUL.values() for item in sublist):
+            forbiddenletters = {'b': 'q', 'j': 'i', 'x': 'k'}
+            for key in forbiddenletters.keys():
+                Translation_Word = Translation_Word.replace(key, forbiddenletters[key])
 
-        print("jop da gibts schon ne übersetzung für :)")
-
-        Translations_Dictionary[English_Word] = get_key(English_Word)
-        print(Translations_Dictionary)
+            print("translated word is", Translation_Word)
+            Translation_Word_Length = len(str(Translation_Word))
+            Hyphens_Length = math.sqrt(Translation_Word_Length)
+            Hyphens_Rounded = int(Hyphens_Length)
+            hyphens = [Translation_Word[i:i + Hyphens_Rounded] for i in range(0, len(Translation_Word), Hyphens_Rounded)]
 
 
-    forbiddenletters = {'b': 'q', 'j': 'i', 'x': 'k'}
-    for key in forbiddenletters.keys():
-        Current_Word = Current_Word.replace(key, forbiddenletters[key])
+            char_amt = [] # how many characters each syllable will have
+            start_chars = [] # list of English syllable start characters for Ozkavosh lookup replacement
 
-    print("translated word is", Current_Word)
-    Current_Word_Length = len(str(Current_Word))
-    Hyphens_Length = math.sqrt(Current_Word_Length)
-    Hyphens_Rounded = int(Hyphens_Length)
-    hyphens = [Current_Word[i:i + Hyphens_Rounded] for i in range(0, len(Current_Word), Hyphens_Rounded)]
+            iterations = len(hyphens)
+            for i in range(iterations):
+                char_amt.append(len(hyphens[i]))
+                start_chars.append(hyphens[i][:1])
 
+            ozk_syllables = [] # list of new Ozkavosh syllables
 
-    char_amt = [] # how many characters each syllable will have
-    start_chars = [] # list of English syllable start characters for Ozkavosh lookup replacement
+            for i in start_chars:
+                character_mapping = dict_charpairs.get(i)
+                weightlist_mapping = weights_dict.get(i)
+                ozk_syllable_result = str(random.choices(population=character_mapping, weights=weightlist_mapping,k=1))  # returns randomly weighted pick from ozk syllables
+                ozk_syllables.append(ozk_syllable_result)
 
-    iterations = len(hyphens)
-    for i in range(iterations):
-        char_amt.append(len(hyphens[i]))
-        start_chars.append(hyphens[i][:1])
+            ozk_word = str(ozk_syllables)
+            ozk_word = [character for character in ozk_word if character.isalnum()]
+            ozk_word = "".join(ozk_word) # get a clean Ozkavosh string for printing
+            print("ozkavosh word is", ozk_word)
 
-    ozk_syllables = [] # list of new Ozkavosh syllables
-
-    for i in start_chars:
-        character_mapping = dict_charpairs.get(i)
-        weightlist_mapping = weights_dict.get(i)
-        ozk_audio_result = str(random.choices(population=character_mapping, weights=weightlist_mapping,k=1))  # returns randomly weighted pick from ozk syllables
-        ozk_syllables.append(ozk_audio_result)
-
-
-    #print(ozk_syllables)
-    ozk_word = str(ozk_syllables)
-    ozk_word = [character for character in ozk_word if character.isalnum()]
-    ozk_word = "".join(ozk_word) # get a clean Ozkavosh string for printing
-    print("ozkavosh word is", ozk_word)
-
-    #Translations_Dictionary.update({English_Word: ozk_word})
-    #print(Translations_Dictionary)
+            Translations_Dictionary.update({English_Word: ozk_word})
+            print(Translations_Dictionary)
 
 
 

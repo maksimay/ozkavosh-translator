b51ad96e54b02421ec02b9d4ab0eec14da650aab
Jannis <jannis@nyx.earth>
2021-02-22 05:22:07 +0100

    trying with .wav

--- generate_dataset.py
+++ generate_dataset.py
@@ -551,7 +551,7 @@ for lines in tqdm(input_text):
     new_sample_rate = int(newaudio.frame_rate * (1.5 ** octaves))
     highpitch_sentence = newaudio._spawn(newaudio.raw_data, overrides={'frame_rate': new_sample_rate})
     highpitch_sentence = highpitch_sentence.set_frame_rate(8000)
-    highpitch_sentence.export("./mycorpus/data/train/" + utt_id + ".wav", format="s16le")
+    highpitch_sentence.export("./mycorpus/data/train/" + utt_id + ".wav", format="wav")
 
     # update kaldi training df
     df2.loc[len(df2.index)] = [file_id, wav_path, speaker_id, utt_id, utt_seg_start, utt_seg_end, oz_transcription]
